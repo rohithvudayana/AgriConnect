de8035a9f50e8faee8fdc784874c5b2e1279bc0d
rohithvudayana <rohithvudayana46@gmail.com>
2023-11-29 08:23:49 +0530

    AgriConnect

---
+++ client/src/Components/AboutUs.js/AboutUs.js
@@ -0,0 +1,58 @@
+import "./AboutUs.css";
+
+function AboutUs() {
+    return (
+        <div className="AboutUs_body">
+
+            <div className="about_main_div">
+                <div className="section">
+                    <div className="container">
+                        <div className="content-section">
+                            <div className="title">
+                                <h1>About Us</h1>
+                            </div>
+                            <div className="content">
+                                <h3>One Stop For all Farmers!</h3>
+                                <p>
+                                    Our Website Major Motto is to Help Farmers in a very unique way.
+                                    Our website consists of five major sections namely Crop
+                                    Suggestion section, Discussions Tab, Market Place for Farmers,
+                                    Login for better services and lastly Chatbot for further
+                                    assistance. For better understanding of the five major
+                                    sections.<br />
+                                    Firstly, Crop Suggestion section the page takes inputs and
+                                    display the best possible crops and their full-fledged details
+                                    such as Season suitable for the crop, minimal investment, water
+                                    availability, Gross income and total profit earned. This, helps
+                                    the Farmers to have better understanding of professional
+                                    farming.<br />
+                                    The website contains Discussion tab in which Farmers can post
+                                    their query and other farmers or experts might help them posting
+                                    solutions to their query.<br />
+                                    The another section of our website and center of attraction is
+                                    the MarketPlace. Farmers can have all the products at one place
+                                    which are required at a very minimal price. machinery, seeds,
+                                    fertilizers, pesticides are some of the basic examples. <br />
+                                    For Better Services Farmers needs to login in to website for
+                                    more assistance.<br />
+                                    Lastly, the Chatbot which helps the users for instant help.
+                                </p>
+                            </div>
+
+                            <div className="social">
+                                <a href=""><i className="fab fa-facebook-f"></i></a>
+                                <a href=""><i className="fab fa-twitter"></i></a>
+                                <a href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                        <div className="image-section">
+                            <img src="./AboutUs_Images/aboutusimg.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
